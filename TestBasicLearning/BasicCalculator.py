bcb342377f9263854724101114856318914e1f11
Umakant P <ukp.linux@gmail.com>
2024-08-15 12:06:44 +0530

    added tested learn code

---
+++ TestBasicLearning/BasicCalculator.py
@@ -0,0 +1,29 @@
+number1 = 100
+number2 = 9
+
+
+def addition(number1, number2):
+    return number1+number2
+
+
+def substraction(number1, number2):
+    return number1-number2
+
+
+def multiplication(number1, number2):
+    return number1*number2
+
+
+def division(number1, number2):
+    return number1/number2
+
+
+def getReminder(number1, number2):
+    return number1 % number2
+
+
+print(addition(number1, number2))
+print(substraction(number1, number2))
+print(division(number1, number2))
+print(multiplication(number1, number2))
+print(getReminder(number1, number2))
---
+++ TestBasicLearning/StringTest.py
@@ -0,0 +1,10 @@
+
+# name = input("Enter user name: ")
+
+# print(f"Hi {name}, Welcome to python code learning...!")
+
+
+stringpara = "The quick brown fox jumps over a lazy dog. here are fox and dog used in para."
+
+# for x in range(10):
+#     print(x)
